d122618628a5a35627702d52ccbc01894253e5f7
SergeiMed <s.medvedev86@mail.ru>
2022-08-06 06:45:30 +0300

    fix App

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,41 +1,65 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
 import java.io.File;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.Callable;
 
-@CommandLine.Command(
-        name="gendiff",
-        description="Compares two configuration files and shows a difference.")
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "Version",
+        description = "Compares two configuration files and shows a difference.")
 
-public class App {
+public class App implements Callable<String> {
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private File file;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private File file1;
+    @Option(names = { "-f", "--format" }, paramLabel = "format", defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format = "stylish";
 
-    @CommandLine.Option(names={"-V", "--version"}, description="Print version information and exit.")
-    boolean version;
-
-    @CommandLine.Option(names={"-h", "--help"}, description="Show this help message and exit.", help=true)
-    boolean help;
-
-    @CommandLine.Option(names = { "-f", "--format" }, paramLabel = "format", defaultValue = "stylish", description = "output format [default: ${DEFAULT-VALUE}]")
-    File file;
-
-    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file", defaultValue = "")
-    private File filepath1;
-
-    @CommandLine.Parameters(paramLabel = "filepath2", description = "path to second file", defaultValue = "")
-    private File filepath2;
-
-    public static void main(String[] args) {
-        final App picocli = CommandLine.populateCommand(new App(), args);
-        if (picocli.help)
-        {
-            CommandLine.usage(picocli, System.out, CommandLine.Help.Ansi.AUTO);
-            return;
+    @Override
+    public final String call() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> scoreByName = mapper.readValue(file, Map.class);
+        Map<String, Object> scoreByName1 = mapper.readValue(file1, Map.class);
+        Map<String, Object> sortedMap = new TreeMap<>(scoreByName);
+        Map<String, Object> sortedMap1 = new TreeMap<>(scoreByName1);
+        StringBuilder builder = new StringBuilder("{\n");
+        for (Map.Entry<String, Object> map : sortedMap.entrySet()) {
+            if (!sortedMap1.containsKey(map.getKey())) {
+                builder.append("- ").append(map.getKey()).append(": ").append(map.getValue()).append("\n");
+            }
+            if (sortedMap1.containsKey(map.getKey())) {
+                if (!map.getValue().equals(sortedMap1.get(map.getKey()))) {
+                    builder.append("- ").append(map.getKey()).append(": ")
+                            .append(map.getValue()).append("\n");
+                    builder.append("+ ").append(map.getKey()).append(": ")
+                            .append(sortedMap1.get(map.getKey())).append("\n");
+                } else if (sortedMap1.containsKey(map.getKey())
+                        && map.getValue().equals(sortedMap1.get(map.getKey()))) {
+                    builder.append("  ").append(map.getKey()).append(": ").append(map.getValue()).append("\n");
+                }
+            }
         }
-        else if (picocli.version)
-        {
-            System.out.println("Version");
-            return;
+        for (Map.Entry<String, Object> map1 : sortedMap1.entrySet()) {
+            if (!sortedMap.containsKey(map1.getKey())) {
+                builder.append("+ ").append(map1.getKey()).append(": ").append(map1.getValue()).append("\n");
+            }
         }
-        System.out.println("Hello World!");
+        builder.append("}\n");
+        String result = builder.toString();
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }

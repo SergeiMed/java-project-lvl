fac0529a96a811699ece56bf40979ad67dc13055
SergeiMed <s.medvedev86@mail.ru>
2022-08-09 07:36:10 +0300

    fix DifferTest

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -1,6 +1,5 @@
-package hexlet.code.DifferTest;
+package hexlet.code;
 
-import hexlet.code.Differ;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +7,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class DifferTest {
+
+class DifferTest {
 
     @Test
     public void testDiffer() throws IOException {
@@ -17,28 +17,28 @@ public class DifferTest {
         FileWriter writer = new FileWriter(file);
         FileWriter writer1 = new FileWriter(file1);
         writer.write("{\n"
-            + "  \"host\": \"hexlet.io\",\n"
-            + "  \"timeout\": 50,\n"
-            + "  \"proxy\": \"123.234.53.22\",\n"
-            + "  \"follow\": false\n"
-            + "}");
+                + "  \"host\": \"hexlet.io\",\n"
+                + "  \"timeout\": 50,\n"
+                + "  \"proxy\": \"123.234.53.22\",\n"
+                + "  \"follow\": false\n"
+                + "}");
         writer1.write("{\n"
-            + "  \"timeout\": 20,\n"
-            + "  \"verbose\": true,\n"
-            + "  \"host\": \"hexlet.io\"\n"
-            + "}");
+                + "  \"timeout\": 20,\n"
+                + "  \"verbose\": true,\n"
+                + "  \"host\": \"hexlet.io\"\n"
+                + "}");
         writer1.flush();
         writer1.close();
         writer.flush();
         writer.close();
         String actual = "{\n"
-            + "- follow: false\n"
-            + "  host: hexlet.io\n"
-            + "- proxy: 123.234.53.22\n"
-            + "- timeout: 50\n"
-            + "+ timeout: 20\n"
-            + "+ verbose: true\n"
-            + "}\n";
+                + "- follow: false\n"
+                + "  host: hexlet.io\n"
+                + "- proxy: 123.234.53.22\n"
+                + "- timeout: 50\n"
+                + "+ timeout: 20\n"
+                + "+ verbose: true\n"
+                + "}";
         Assertions.assertEquals(Differ.generate(file, file1), actual);
     }
 }

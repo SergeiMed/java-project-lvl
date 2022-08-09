87641a3ca64adf285f480f9de08d06485dcc7948
SergeiMed <s.medvedev86@mail.ru>
2022-08-09 07:34:09 +0300

    fix Differ

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,43 +1,62 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.concurrent.Callable;
 
-public class Differ {
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "Version",
+        description = "Compares two configuration files and shows a difference.")
+
+public class Differ implements Callable<String> {
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private File file;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private File file1;
+    @Option(names = { "-f", "--format" }, paramLabel = "format", defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format = "stylish";
+
+    @Override
+    public final String call() throws Exception {
+        return generate(file, file1);
+    }
+
+    public static void main(String[] args) {
+        new CommandLine(new Differ()).execute(args);
+    }
 
     public static String generate(File file1, File file2) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> scoreByName = mapper.readValue(file1, Map.class);
-        Map<String, Object> scoreByName1 = mapper.readValue(file2, Map.class);
-        Map<String, Object> sortedMap = new TreeMap<>(scoreByName);
-        Map<String, Object> sortedMap1 = new TreeMap<>(scoreByName1);
+        Map<String, Object> map1 = Parser.parser(file1);
+        Map<String, Object> map2 = Parser.parser(file2);
         StringBuilder builder = new StringBuilder("{\n");
-        for (Map.Entry<String, Object> map : sortedMap.entrySet()) {
-            if (!sortedMap1.containsKey(map.getKey())) {
+        for (Map.Entry<String, Object> map : map1.entrySet()) {
+            if (!map2.containsKey(map.getKey())) {
                 builder.append("- ").append(map.getKey()).append(": ").append(map.getValue()).append("\n");
             }
-            if (sortedMap1.containsKey(map.getKey())) {
-                if (!map.getValue().equals(sortedMap1.get(map.getKey()))) {
+            if (map2.containsKey(map.getKey())) {
+                if (!map.getValue().equals(map2.get(map.getKey()))) {
                     builder.append("- ").append(map.getKey()).append(": ")
-                        .append(map.getValue()).append("\n");
+                            .append(map.getValue()).append("\n");
                     builder.append("+ ").append(map.getKey()).append(": ")
-                        .append(sortedMap1.get(map.getKey())).append("\n");
-                } else if (sortedMap1.containsKey(map.getKey())
-                    && map.getValue().equals(sortedMap1.get(map.getKey()))) {
+                            .append(map2.get(map.getKey())).append("\n");
+                } else if (map2.containsKey(map.getKey())
+                        && map.getValue().equals(map2.get(map.getKey()))) {
                     builder.append("  ").append(map.getKey()).append(": ").append(map.getValue()).append("\n");
                 }
             }
         }
-        for (Map.Entry<String, Object> map1 : sortedMap1.entrySet()) {
-            if (!sortedMap.containsKey(map1.getKey())) {
-                builder.append("+ ").append(map1.getKey()).append(": ").append(map1.getValue()).append("\n");
+        for (Map.Entry<String, Object> map3 : map2.entrySet()) {
+            if (!map1.containsKey(map3.getKey())) {
+                builder.append("+ ").append(map3.getKey()).append(": ").append(map3.getValue()).append("\n");
             }
         }
-        builder.append("}\n");
+        builder.append("}");
         String result = builder.toString();
         System.out.println(result);
         return result;

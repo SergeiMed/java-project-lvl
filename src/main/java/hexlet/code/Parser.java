7024c653dd0cf29aa6c513f7561788ec1abab8ce
SergeiMed <s.medvedev86@mail.ru>
2022-08-19 07:21:30 +0300

    fix Parser

--- src/main/java/hexlet/code/Parser.java
+++ src/main/java/hexlet/code/Parser.java
@@ -3,17 +3,13 @@ package hexlet.code;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Parser {
 
-    public static Map<String, Object> parser(File file) throws IOException {
+    public static Map<String, Object> parser(String string) throws IOException {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        Map<String, Object> yml1 = mapper.readValue(file, Map.class);
-        Map<String, Object> sortedMap = new TreeMap<>(yml1);
-        return sortedMap;
+        return mapper.readValue(string, Map.class);
     }
 }
--- src/test/java/hexlet/code/ParserTest.java
+++ src/test/java/hexlet/code/ParserTest.java
@@ -3,7 +3,6 @@ package hexlet.code;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,11 +13,18 @@ public class ParserTest {
 
     @Test
     public void parserTest() throws IOException {
+        String json = """
+                {
+                  "host": "hexlet.io",
+                  "timeout": 50,
+                  "proxy": "123.234.53.22",
+                  "follow": false
+                }""";
         Map<String, Object> expect = new HashMap<>();
         expect.put("host", "hexlet.io");
         expect.put("timeout", testNumber);
         expect.put("proxy", "123.234.53.22");
         expect.put("follow", false);
-        Assertions.assertEquals(expect, Parser.parser(new File("./src/test/resources/file.json")));
+        Assertions.assertEquals(expect, Parser.parser(json));
     }
 }

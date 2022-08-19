d23cfb411bf94ed8e8cac3c03fba2bbc89f3ceeb
SergeiMed <s.medvedev86@mail.ru>
2022-08-19 07:22:22 +0300

    add format Stylish

---
+++ src/main/java/hexlet/code/formats/Stylish.java
@@ -0,0 +1,46 @@
+package hexlet.code.formats;
+
+import hexlet.code.ValueInfo;
+
+import java.util.Map;
+
+public class Stylish {
+
+    public static String stylish(Map<String, ValueInfo<Object>> map) throws Exception {
+        StringBuilder builder = new StringBuilder("{\n");
+        for (Map.Entry<String, ValueInfo<Object>> entry : map.entrySet()) {
+            switch (entry.getValue().getStatus()) {
+                case "changed" -> {
+                    builder.append("  - ")
+                            .append(entry.getKey())
+                            .append(": ")
+                            .append(entry.getValue().getFirstValue())
+                            .append("\n");
+                    builder.append("  + ")
+                            .append(entry.getKey())
+                            .append(": ")
+                            .append(entry.getValue().getSecondValue())
+                            .append("\n");
+                }
+                case "unchanged" -> builder.append("    ")
+                        .append(entry.getKey())
+                        .append(": ")
+                        .append(entry.getValue().getFirstValue())
+                        .append("\n");
+                case "new" -> builder.append("  + ")
+                        .append(entry.getKey())
+                        .append(": ")
+                        .append(entry.getValue().getSecondValue())
+                        .append("\n");
+                case "delete" -> builder.append("  - ")
+                        .append(entry.getKey())
+                        .append(": ")
+                        .append(entry.getValue().getFirstValue())
+                        .append("\n");
+                default -> throw new Exception("incorrect status");
+            }
+        }
+        builder.append("}");
+        return builder.toString();
+    }
+}

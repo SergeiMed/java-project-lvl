822d26339cfceea5129505b3229858b794a84bf8
SergeiMed <s.medvedev86@mail.ru>
2022-08-20 17:03:20 +0300

    add format json

---
+++ src/main/java/hexlet/code/formats/Json.java
@@ -0,0 +1,16 @@
+package hexlet.code.formats;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.ValueInfo;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class Json {
+
+    public static String json(Map<String, ValueInfo<Object>> map) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+    }
+}
